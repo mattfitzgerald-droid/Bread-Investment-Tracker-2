c8e3d910ba65f468063f8ccd61e93f960e108fac
mattfitzgerald-droid <matt.fitzgerald@beyondbettermarketing.com>
2026-05-25 15:39:47 -0400

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -3,7 +3,7 @@ export const trackerConfig = {
   ticker: "QQQ",
   // Change this to the date you want the imaginary investment to begin.
   // Format: YYYY-MM-DD
-  purchaseDate: "2024-12-31",
+  purchaseDate: "2026-05-23",
   appName: "Bread Investment Tracker",
   tagline: "What if the bread settlement money went full tech bro?"
 };
